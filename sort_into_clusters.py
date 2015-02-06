f45e84d7dec3c4ab289a63038f6f118e6022afee
JanoHorvath <jano.horvath@gmail.com>
2015-02-06 20:49:29 +0000

    Refectored color assignment and cluster centers creation into a method. Added new method for recalibrating cluster centers after color assignment.

--- sort_into_clusters.py
+++ sort_into_clusters.py
@@ -4,30 +4,50 @@ from random import randint
 
 
 class SortIntoClusters():
-    number_of_clusters = raw_input('Enter number of clusters: ')
-    size_of_graph = raw_input('Enter size of graph: ')
-
     dataset = Dataset()
     cluster_center = []
+    size_of_graph = 500
+
     colors = ['red', 'blue', 'green', 'pink', 'brown', 'grey', 'gold', 'purple']
 
     Points = dataset.get_mock_dataset(dataset, 50)
 
-    """Initialize n cluster centers"""
-    for n in number_of_clusters:
-        cluster_center[n] = [randint(0, size_of_graph), randint(0, size_of_graph), colors[n]]
+    def initialize(self):
+        number_of_clusters = raw_input('Enter number of clusters: ')
+        self.size_of_graph = raw_input('Enter size of graph: ')
 
-    """Assign color to every point"""
-    for point in Points:
-        distance = size_of_graph*2
+        """Initialize n cluster centers"""
+        for n in number_of_clusters:
+            self.cluster_center[n] = [randint(0, self.size_of_graph), randint(0, self.size_of_graph), self.colors[n]]
 
-        """by computing distance to every cluster center"""
-        for cluster in cluster_center:
-            sum = 0
-            for x in range(0, len(point)-1):
-                sum += math.pow(point[x]-cluster[x], 2)
+    def assign_points(self):
+        """Assigns color to every point"""
 
-            """and finding the closest one."""
-            new_distance = math.pow(sum, 1/2)
-            if new_distance < distance:
-                point[-1] = cluster[-1]
+        for point in self.Points:
+            distance = self.size_of_graph*2
+
+            """by computing distance to every cluster center"""
+            for cluster in self.cluster_center:
+                sum = 0
+                for x in range(0, len(point)-1):
+                    sum += math.pow(point[x]-cluster[x], 2)
+
+                """and finding the closest one."""
+                new_distance = math.pow(sum, 1/2)
+                if new_distance < distance:
+                    point[-1] = cluster[-1]
+
+    def recalibrate(self):
+        """ Repositions cluster centers """
+
+        for center in self.cluster_center:
+            average = []
+
+            """by getting all the points of the same color"""
+            for point in filter(lambda p: p[-1] == center[-1], self.Points):
+                for i in range(0, len(point)-1):
+                    average[i] += point[i]
+
+            """and computing the average of all dimensions."""
+            for i in range(0, len(center)-1):
+                center[i] = average[i] / len(average)
