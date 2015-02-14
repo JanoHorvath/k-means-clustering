60158f150d0a8257d1c27df1be53b1dfe4b3a0b2
JanoHorvath <jano.horvath@gmail.com>
2015-02-14 15:13:43 +0000

    Created a separate method for analyzing and merging cluster centers.

--- sort_into_clusters.py
+++ sort_into_clusters.py
@@ -10,10 +10,11 @@ class SortIntoClusters():
         self.graph_height = 500
         self.graph_width = 500
         self.colors = ['red', 'dodger blue', 'green2', 'deep pink', 'brown', 'gray', 'gold', 'purple', 'cyan', 'green4']
+        #'yellow', 'orange', 'salmon pink', 'light brown']
         self.Points = []
 
     def initialize_data(self, canvas):
-        """ Get info about canvas and initialize 20 points"""
+        """ Get info about canvas and initialize 50 points"""
         self.graph_height = canvas.winfo_reqwidth()-30
         self.graph_width = canvas.winfo_reqheight()-30
         self.cluster_center = []
@@ -81,6 +82,55 @@ class SortIntoClusters():
         print('Repositioned Cluster Centers')
         return old_centers == self.cluster_center
 
+    def analyze(self, all_cluster_centers):
+        """ Analyses the data, outputting the most frequent cluster center coordinates. """
+
+        deviation = 5
+
+        """ First merges centers close to each other counting how many were merged together, stripping color attribute"""
+        final_cluster_centers = [all_cluster_centers[0][:-1]]
+        final_cluster_centers[0].append(1)
+
+        for cluster in all_cluster_centers:
+            cluster[-1] = 1 #this will be the count of how many centers are merged to one (weight)
+            duplicate = True
+            for final_cluster in final_cluster_centers:
+                for i in range(0, len(cluster)-2):
+                    if (cluster[i] > (final_cluster[i] + deviation)) or (cluster[i] < (final_cluster[i] - deviation)):
+                        duplicate = False
+                        break
+                    else:
+                        duplicate = True
+                if duplicate:
+                    final_cluster[-1] += 1
+                    break
+
+            if not duplicate:
+                final_cluster_centers.append(cluster)
+
+        """ Compute mean weight of merged cluster centers """
+        sum = 0
+        all_the_weight = []
+        for final_cluster in final_cluster_centers:
+            sum += final_cluster[-1]
+            all_the_weight.append(final_cluster[-1])
+        mean_occurence = sum/len(final_cluster_centers)
+        print ('Mean : ', mean_occurence)
+        print ('All of them: ', all_the_weight)
+
+        """ Filter out those cluster centers, which are greater than the popular threshold (weight) """
+        popular_threshold = mean_occurence * 2
+        final_cluster_centers = filter(lambda p: p[-1] > popular_threshold, final_cluster_centers)
+
+        """  and reassign colors. """
+        for n in range(0, len(final_cluster_centers)):
+            final_cluster_centers[n].append(self.colors[n%10])
+            print(final_cluster_centers[n][-2:])
+
+        # """ Then compute the mean distance of a merged cluster to the points assigned to it"""
+
+        return final_cluster_centers
+
     def render(self, canvas):
         if len(self.Points[0]) <= 3:
             print('Rendering 2D.')
@@ -119,51 +169,23 @@ class SortIntoClusters():
         print('Done')
 
     def detectClusters(self, canvas, number_of_repetitions):
-        """ Repeats the algorithm n times with 2-10 cluster centers """
+        """ Repeats the algorithm n times with 5-10 cluster centers """
         all_cluster_centers = []
-        deviation = 5
-
-        for i in range(2, 10):
-            print('Starting with' + str(i) + ' centers')
+        for i in range(5, 10):
+            print('Starting with ' + str(i) + ' centers')
             for _ in range(0, int(number_of_repetitions)):
                 self.clusterizeStep(canvas, i)
                 for center in self.cluster_center:
                     all_cluster_centers.append(center)
+            print('Ran the algorithm ' + str(number_of_repetitions) + ' times.')
 
-        """ Now analyse the coordinates of clusters adding those inside deviation together, stripping away last parameter (color)"""
-        final_cluster_centers = [all_cluster_centers[0][:-1]]
-        final_cluster_centers[0].append(1)
-
-        for cluster in all_cluster_centers:
-            cluster[-1] = 1 #this will be the count of how many clusters are approximated to a center
-            duplicate = True
-            for final_cluster in final_cluster_centers:
-                for i in range(0, len(cluster)-2):
-                    if (cluster[i] > (final_cluster[i] + deviation)) or (cluster[i] < (final_cluster[i] - deviation)):
-                        duplicate = False
-                        break
-                    else:
-                        duplicate = True
-                if duplicate:
-                    final_cluster[-1] += 1
-                    break
-
-            if not duplicate:
-                final_cluster_centers.append(cluster)
-
-        print('Merged results into ' + str(len(final_cluster_centers)) + ' new final centers.')
-
-        """ Filter out the most popular final_cluster_centers and reassign colors """
-        popular_treshold = 5
-        final_cluster_centers = filter(lambda p: p[-1] > popular_treshold, final_cluster_centers)
-
-        for n in range(0, len(final_cluster_centers)):
-            final_cluster_centers[n].append(self.colors[n%10])
-            print(final_cluster_centers[n][-2:])
+        """ and then filters out the results. """
+        final_cluster_centers = self.analyze(all_cluster_centers)
+        print('Merged ' + str(len(all_cluster_centers)) + ' centers into ' + str(len(final_cluster_centers)) + ' new final centers.')
 
         self.cluster_center = final_cluster_centers
         self.assign_points()
-        #self.cluster_center = [] #hack to only color point in the final step
+        #self.cluster_center = [] #hack to hide cluster centers in the final.
         self.render(canvas)
 
         print('Done.')
